cb5f27212712d7c47855e2b3b9e8830ca3b06d4a
Fredrik Savje <fred_gh@savje.com>
2017-02-13 19:46:19 -0800

    Update sim settings

--- complexity/gen_batches.R
+++ complexity/gen_batches.R
@@ -16,23 +16,20 @@ n_rounds <- as.integer(get_config("NROUNDS", "env.sh"))
 set.seed(06041986)
 
 # opt_kmatch
-level1_sample_sizes <- c("2e1", "1e2", "5e2", "10e2", "25e2", "50e2",
-                         "75e2", "100e2", "125e2")
+level1_sample_sizes <- c("2e1", "1e2", "5e2", "1e3", "5e3", "10e3")
 
 # opt_pairmatch
-level2_sample_sizes <- c("150e2", "175e2", "200e2", "225e2")
+level2_sample_sizes <- c("15e3", "20e3")
 
 # opt_fullmatch
-level3_sample_sizes <- c("250e2", "275e2", "300e2", "325e2", "350e2", "375e2", "400e2")
+level3_sample_sizes <- c("25e3", "30e3", "35e3", "40e3")
 
 # gre_kmatch gre_pairmatch rep_pairmatch
-level4_sample_sizes <- c("425e2", "450e2", "475e2", "500e2",
-                         "6e4", "7e4", "8e4", "9e4", "10e4", "15e4",
-                         "20e4", "25e4", "30e4")
+level4_sample_sizes <- c("45e3", "50e3",
+                         "10e4", "15e4", "20e4", "25e4", "30e4")
 
 # scclust_EXU_CSE scclust_LEX_ANY
-level5_sample_sizes <- c("35e4", "40e4", "45e4", "50e4", "55e4", "60e4", "65e4",
-                         "70e4", "75e4", "80e4", "85e4", "90e4", "95e4", "10e5",
+level5_sample_sizes <- c("35e4", "40e4", "45e4", "50e4", "10e5", "50e5",
                          "1e7", "2e7", "3e7", "4e7", "5e7", "6e7", "7e7",
                          "8e7", "9e7", "10e7")
 
