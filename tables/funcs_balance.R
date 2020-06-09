dbde1c8593f9d2d3fa3a70a609b97fd467c70958
Fredrik Savje <fred_gh@savje.com>
2020-06-09 12:45:44 +0200

    Remove headers

--- tables/funcs_balance.R
+++ tables/funcs_balance.R
@@ -6,8 +6,7 @@ make_sub_table <- function(compiled_results,
                            cols_to_normalize = NULL,
                            normalize_with = NULL,
                            digits = 2,
-                           nsmall = 2,
-                           col_labels = names(stats_cols)) {
+                           nsmall = 2) {
   tmp_results <- compiled_results[compiled_results$sample_size == ss &
                                     compiled_results$method %in% methods_include,
                                   c("method", stats_cols)]
@@ -20,10 +19,7 @@ make_sub_table <- function(compiled_results,
                     function(col) {
                       format(col, justify = "right", digits = digits, nsmall = nsmall, scientific = FALSE)
                     })
-  col_labels <- paste0("\\multicolumn{1}{r}{", col_labels, "}")
-  tmp_mat <- format(rbind(col_labels, rep(" ", length(col_labels)), tmp_mat), justify = "right")
   tmp_mat <- apply(tmp_mat, 1, paste, collapse = " & ")
-  tmp_mat <- format(c(paste0("\\multicolumn{", length(stats_cols), "}{c}{", title, "}"), tmp_mat), justify = "centre")
   tmp_mat <- paste("& &", tmp_mat)
   tmp_mat <- sub("\\\\", " \\\\", tmp_mat)
   matrix(tmp_mat, ncol = 1)
@@ -32,17 +28,10 @@ make_sub_table <- function(compiled_results,
 
 save_table <- function(file,
                        res_mat,
-                       row_labels,
-                       cline_add = NULL) {
-  res_mat <- cbind(format(c(" ", " ", " ", row_labels), justify = "left"),
+                       row_labels) {
+  res_mat <- cbind(format(row_labels, justify = "left"),
                    res_mat)
   res_mat <- apply(res_mat, 1, paste, collapse = " ")
-  res_mat <- paste(res_mat, " \\\\ ")
-  res_mat[1] <- paste(res_mat[1], "[0.1cm]")
-  res_mat[2] <- paste(res_mat[2], "[0.15cm]")
-  res_mat[3] <- paste(res_mat[3], "[-0.6cm]")
-  if (!is.null(cline_add)) {
-    res_mat[2] <- paste(res_mat[2], cline_add)
-  }
-  cat(paste(res_mat, collapse = "\n"), file = file)
+  res_mat <- paste(res_mat, " \\\\")
+  cat(paste(res_mat, collapse = "\n"), "\n", file = file, sep = "")
 }
